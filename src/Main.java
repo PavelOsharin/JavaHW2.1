06e191c7134c6133601f22cd28f5867292c1971f
Pavel Osharin <mr.osharin@mail.ru>
2023-08-30 20:19:35 +0300

    Renaming variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 25_676; //стоимость билета
-        int y = 20; // количетсво рублей за которые начисляется 1 миля
-        int z = x / y; // количество миль
+        int TicketPrice = 25_676; //стоимость билета
+        int BonusMoney = 20; // количетсво рублей за которые начисляется 1 миля
+        int Miles = TicketPrice / BonusMoney; // количество миль
 
-        System.out.println(z + " бонусных миль");
+        System.out.println(Miles + " бонусных миль");
     }
 }
